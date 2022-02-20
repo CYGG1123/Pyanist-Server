9b550518ae7dba78f78f262807cb14c03049be0b
ElderBacon <true.winger_0i@icloud.com>
2022-02-20 23:27:42 +0800

    fix: :bug: 半修复了TCP粘包，但还是会偶发

--- src/dataTrans.cpp
+++ src/dataTrans.cpp
@@ -1,39 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <iostream>
-#include "getTime.h"
+#include "dataTrans.h"
 
 using namespace std;
 
-void *receiveMsg(void *sock) {
-    system("pwd");
-    FILE *fp = fopen(("../Received/" + getCurrentTime() + ".jpg").c_str(), "wb");
-    if (fp == NULL) {
-        cout << "file not found" << endl;
+struct PacketHeader {
+    int size;            // 大小：字节
+    PacketHeader() {
+        size = 0;
     }
+};
 
-    char buffer[4096];
+void *receiveMsg(void *sock) {
+    // system("pwd");
+    char buffer[1024];
     int *socket = (int *) sock;
     while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        ssize_t byteCount = recv(*socket, buffer, 4096, 0);
-        if (byteCount <= 0) {
+        usleep(500);
+        int nRecv = recv(*socket, buffer, 1024, 0);
+        if (nRecv <= 0) {
             continue;
+        } else if (nRecv == sizeof(PacketHeader)) {
+            PacketHeader ph;
+            memcpy(&ph, buffer, nRecv);
+            cout << "大小：" << ph.size << "Bytes" << endl;
+            string filename = ("../Received/" + getCurrentTime() + ".jpg");
+            FILE *fp = fopen(filename.c_str(), "wb");
+            if (fp == NULL) {
+                cout << "file not found" << endl;
+            }
+            cout << "开始接收图片" << endl;
+            nRecv = 0;
+            while (nRecv < ph.size) {
+                memset(buffer, 0, sizeof(buffer));
+                int byteCount = recv(*socket, buffer, 1024, 0);
+                if (byteCount <= 0) {
+                    continue;
+                }
+                fwrite(buffer, 1, byteCount, fp);
+                nRecv += byteCount;
+                cout << "已接收 " << nRecv << " Bytes" << endl;
+            }
+            cout << "共接收 " << nRecv << " Bytes" << endl;
+            fclose(fp);
+        } else {
+            buffer[nRecv] = '\0';
+            cout << buffer << endl;
         }
-        fwrite(buffer, 1, byteCount, fp);
     }
+    close(*socket);
+    // string command = "python3 PaddleOCR.py " + filename;
+    // system(command.c_str());
 }
 
 void *sendMsg(void *sock) {
     while (1) {
         usleep(500);
-        char buffer[4096];
+        char buffer[1024];
         memset(buffer, 0, sizeof(buffer));
         cin >> buffer;
         if (strlen(buffer) < 0) {
--- src/dataTrans.h
+++ src/dataTrans.h
@@ -4,7 +4,17 @@
 
 #ifndef SERVER_DATATRANS_H
 #define SERVER_DATATRANS_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include "getTime.h"
+#include <string>
 void *sendMsg(void *sock);
 
 void *receiveMsg(void *sock);
