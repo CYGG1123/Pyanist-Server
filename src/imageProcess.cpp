b4c1dca1798c2817cefb901f19d742e6f060ca84
ElderBacon <true.winger_0i@icloud.com>
2022-03-02 15:10:31 +0800

    fix: :bug: 修复图片读取的路径

--- src/imageProcess.cpp
+++ src/imageProcess.cpp
@@ -1,24 +1,28 @@
 //
-// Created by ELderBacon on 2022/2/5.
+// Created by ElderBacon on 2022/2/5.
 //
 #include "imageProcess.h"
 
-
 string imageProcess(const string &filename) {
-    char result[1024];
-    //    executeCMD(("python3 src/imageProcess.py " + filename).c_str(), result);
-    system("pwd");
-    executeCMD(("python3 ../src/imageProcess.py -HelloWorld"), result);
-    string str;
-    //字符串处理有点问题
-    for (int i = 0; i < sizeof(result); ++i) {
-        if (result[i] == '\0') {
-            break;
-        } else if ((result[i] >= 'A' && result[i] <= 'Z') ||
-                   (result[i] >= 'a' && result[i] <= 'z') ||
-                   result[i] >= '0' && result[i] <= '9') {
-            str.push_back(result[i]);  //提取出来所有乐谱的成分
-        } else { continue; }
-    }
-    return str;
+   char result[1024];
+   // system("pwd");
+   // executeCMD(("python3 ../src/imageProcess.py -HelloWorld"), result);
+   executeCMD(("python3 ../src/imageProcess.py " + string(filename)).c_str(), result);
+
+
+   string str;
+   //字符串处理有点问题
+   for (int i = 0; i < sizeof(result); ++i) {
+       if (result[i] == '\0') {
+           break;
+       } else if ((result[i] >= 'A' && result[i] <= 'Z') ||
+                  (result[i] >= 'a' && result[i] <= 'z') ||
+                  result[i] >= '0' && result[i] <= '7') {
+           str.push_back(result[i]); //提取出来所有乐谱的成分
+       } else {
+           continue;
+       }
+   }
+   return str;
+    // return "HelloWorld";
 }
--- src/imageProcess.py
+++ src/imageProcess.py
@@ -2,8 +2,38 @@
 
 # -*- coding:utf-8 -*-
 
+import cv2
+import paddlehub as hub
 import sys
-print('第一个参数:%s' % sys.argv[0])
-if sys.argv[1].startswith('-'):
-    c1 = sys.argv[1][1:]
-    print(c1)
+import os
+# print('第一个参数:%s' % sys.argv[0])
+# if sys.argv[1].startswith('-'):
+#     c1 = sys.argv[1][1:]
+#     print(c1)
+
+# os.system("pwd");
+# 待预测图片
+test_img_path = ["../Received/"+sys.argv[1]]  # 我草，你的路径为什么会自己跳到上一级的？
+# 加载移动端预训练模型
+# ocr = hub.Module(name="chinese_ocr_db_crnn_mobile")
+# 服务端可以加载大模型，效果更好
+ocr = hub.Module(name="chinese_ocr_db_crnn_server")
+
+
+# 读取测试文件夹test.txt中的照片路径
+np_images = [cv2.imread(image_path) for image_path in test_img_path]
+
+results = ocr.recognize_text(
+    images=np_images,         # 图片数据，ndarray.shape 为 [H, W, C]，BGR格式；
+    use_gpu=False,            # 是否使用 GPU；若使用GPU，请先设置CUDA_VISIBLE_DEVICES环境变量
+    output_dir='ocr_result',  # 图片的保存路径，默认设为 ocr_result；
+    visualization=True,       # 是否将识别结果保存为图片文件；
+    box_thresh=0.5,           # 检测文本框置信度的阈值；
+    text_thresh=0.5)          # 识别中文文本置信度的阈值；
+
+for result in results:
+    data = result['data']
+    save_path = result['save_path']
+    for infomation in data:
+        # print('text: ', infomation['text'], '\nconfidence: ', infomation['confidence'], '\ntext_box_position: ', infomation['text_box_position'])
+        print(infomation['text'])
